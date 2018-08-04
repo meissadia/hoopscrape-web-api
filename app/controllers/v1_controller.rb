de740a6c997e4e986547f585196b85d633899177
Meissa Dia <meissadia@gmail.com>
2018-08-04 12:36:08 -0600

    init

---
+++ app/controllers/v1_controller.rb
@@ -0,0 +1,30 @@
+class V1Controller < ApplicationController
+  before_action :set_id
+
+  def teams
+    render json: @hs.teamList
+  end
+
+  def player
+    render json: @hs.player(@id)
+  end
+
+  def schedule
+    render json: @hs.schedule(@id)
+  end
+
+  def roster
+    render json: @hs.roster(@id)
+  end
+
+  def boxscore
+    render json: @hs.boxscore(@id)
+  end
+  
+  private
+
+  def set_id
+    @id = params[:id]
+    @hs = HoopScrape.new(format: :to_hashes)
+  end
+end
